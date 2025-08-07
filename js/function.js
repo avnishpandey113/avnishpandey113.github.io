bb0b95910195a983015e6be3e174ae46636e7b53
avnishpandey113 <45109503+avnishpandey113@users.noreply.github.com>
2025-08-07 16:44:17 -0600

    Add Jest testing setup and scrollspy refresh

--- js/function.js
+++ js/function.js
@@ -1,3 +1,9 @@
-// $('[data-spy="scroll"]').each(function () {
-//   var $spy = $(this).scrollspy('refresh')
-// })
+function refreshScrollSpy() {
+  $('[data-spy="scroll"]').each(function () {
+    $(this).scrollspy('refresh');
+  });
+}
+
+if (typeof module !== 'undefined') {
+  module.exports = { refreshScrollSpy };
+}
